d5ff1dd914878158160d00604cf65456d137e789
crthc <>
2020-06-09 08:40:23 +0200

    no message

--- Rockets/Controllers/controller.ts
+++ Rockets/Controllers/controller.ts
@@ -1,24 +1,10 @@
 let rocket1: Rocket;
 let rocket2: Rocket;
 
-(document.getElementById("rocket1") as HTMLInputElement).addEventListener('click', createrocket1);
-(document.getElementById("rocket2") as HTMLInputElement).addEventListener('click', createrocket2);
-(document.getElementById("accelerateRocket1") as HTMLInputElement).addEventListener('click', accelerateRocket1);
-(document.getElementById("accelerateRocket2") as HTMLInputElement).addEventListener('click', accelerateRocket2);
-(document.getElementById("breakRocket1") as HTMLInputElement).addEventListener('click', breakRocket1);
-(document.getElementById("breakRocket2") as HTMLInputElement).addEventListener('click', breakRocket2);
-(document.getElementById("printRocket1") as HTMLInputElement).addEventListener('click', printRocket1);
-(document.getElementById("printRocket2") as HTMLInputElement).addEventListener('click', printRocket2);
-(document.getElementById("printAll") as HTMLInputElement).addEventListener('click', () => {
-    printRocket1();
-    printRocket2();
-});
-
-function createrocket1(){
-    let createRocket = document.getElementById("rocket1") as HTMLInputElement;
-    createRocket.disabled = true;
-    createRocket.innerHTML = 'Created!';
-
+(document.getElementById("rocket1") as HTMLInputElement).addEventListener('click', () =>{ // Se crea rocket1 en esta función 
+    (document.getElementById("rocket1") as HTMLInputElement).disabled = true;
+    (document.getElementById("rocket1") as HTMLInputElement).innerHTML = 'Created!';
+   
     let id = '32WESSDS';
     rocket1 = new Rocket(id);
 
@@ -30,11 +16,10 @@ function createrocket1(){
         rocket1.addThruster(thrusters);
     }
 
-    console.log(rocket1);
+});
 
-}
+(document.getElementById("rocket2") as HTMLInputElement).addEventListener('click', () =>{ // Se crea rocket2 en esta función
 
-function createrocket2(){
     let createRocket = document.getElementById("rocket2") as HTMLInputElement;
     createRocket.disabled = true;
     createRocket.innerHTML = 'Created!';
@@ -49,10 +34,33 @@ function createrocket2(){
         let thrusters = new Thruster(maxPower[i]); 
         rocket2.addThruster(thrusters);
     }
-}
+});
 
-function accelerateRocket1(){
-    for(let i of rocket1.thrusters){
+(document.getElementById("accelerateRocket1") as HTMLInputElement).addEventListener('click', () =>{
+    accelerateRocket(rocket1);
+});
+(document.getElementById("accelerateRocket2") as HTMLInputElement).addEventListener('click', () => {
+    accelerateRocket(rocket2);
+});
+(document.getElementById("breakRocket1") as HTMLInputElement).addEventListener('click', () => {
+    breakRocket(rocket1);
+});
+(document.getElementById("breakRocket2") as HTMLInputElement).addEventListener('click', () => {
+    breakRocket(rocket2);
+});
+(document.getElementById("printRocket1") as HTMLInputElement).addEventListener('click', () => {
+    (document.getElementById("showRocket1") as HTMLParagraphElement).innerHTML = printRocket(rocket1);
+});
+(document.getElementById("printRocket2") as HTMLInputElement).addEventListener('click', () => {
+    (document.getElementById("showRocket2") as HTMLParagraphElement).innerHTML = printRocket(rocket2);
+});
+(document.getElementById("printAll") as HTMLInputElement).addEventListener('click', () => {
+    (document.getElementById("showRocket1") as HTMLParagraphElement).innerHTML = printRocket(rocket1);
+    (document.getElementById("showRocket2") as HTMLParagraphElement).innerHTML = printRocket(rocket2);
+});
+
+function accelerateRocket(rocket:any){
+    for(let i of rocket.thrusters){
         if (i.actualPower == i.maxPower){
             i.actualPower = i.actualPower;
         }else if (i.actualPower < i.maxPower){
@@ -61,19 +69,8 @@ function accelerateRocket1(){
     }
 }
 
-function accelerateRocket2(){
-    for(let i of rocket2.thrusters){
-        if (i.actualPower == i.maxPower){
-            i.actualPower = i.actualPower;
-        }else if (i.actualPower < i.maxPower){
-            i.accelerate();
-        }   
-    }
-    
-}
-
-function breakRocket1(){
-    for(let i of rocket1.thrusters){
+function breakRocket(rocket:any){
+    for(let i of rocket.thrusters){
         if (i.actualPower == 0){
             i.actualPower = i.actualPower;
         }else if (i.actualPower > 0){
@@ -82,55 +79,26 @@ function breakRocket1(){
     }
 }
 
-function breakRocket2(){
-    for(let i of rocket2.thrusters){
-        if (i.actualPower == 0){
-            i.actualPower = i.actualPower;
-        }else if (i.actualPower > 0){
-            i.break();
-        }   
-    }
-}
-
-function printRocket1(){
-
+function printRocket(rocket:any){
      // Sacamos potencias de cada propulsor.
     let thrustersPower = []
-    for(let i of rocket1.thrusters){
+    for(let i of rocket.thrusters){
         thrustersPower.push(i.maxPower);
     }
 
     let thrustersActualPower = []
-    for(let i of rocket1.thrusters){
+    for(let i of rocket.thrusters){
         thrustersActualPower.push(i.actualPower);
     }
     
     let totalmaxPower = thrustersPower.reduce((a, b) => a + b, 0); // Potencia máxima total del cohete
     let actualPower = thrustersActualPower.reduce((a, b) => a + b, 0); // Potencia actual del cohete
  
-    let showRocket = document.getElementById("showRocket1") as HTMLParagraphElement; 
-    showRocket.innerHTML= `Rocket ${rocket1.id} actual power is ${actualPower}. <br>The ${rocket1.thrusters.length} thrusters actual power is ${thrustersActualPower.join(', ')}. <br>Rocket maximum power is ${totalmaxPower}. <br>Thrusters maximum power is ${thrustersPower.join(', ')}.`; 
-    
+    return `Rocket ${rocket.id} actual power is ${actualPower}. <br>The ${rocket.thrusters.length} thrusters actual power is ${thrustersActualPower.join(', ')}. <br>Rocket maximum power is ${totalmaxPower}. <br>Thrusters maximum power is ${thrustersPower.join(', ')}.`; 
+
 }
 
-function printRocket2(){
-     // Sacamos potencias de cada propulsor.
-    let thrustersPower = []
-    for(let i of rocket2.thrusters){
-        thrustersPower.push(i.maxPower);
-    }
 
-    let thrustersActualPower = []
-    for(let i of rocket2.thrusters){
-        thrustersActualPower.push(i.actualPower);
-    }
-
-    let totalmaxPower = thrustersPower.reduce((a, b) => a + b, 0); // Potencia máxima total del cohete
-    let actualPower = thrustersActualPower.reduce((a, b) => a + b, 0); // Potencia actual del cohete
-    
-    let showRocket = document.getElementById("showRocket2") as HTMLParagraphElement; 
-    showRocket.innerHTML= `Rocket ${rocket2.id} actual power is ${actualPower}. <br>The ${rocket2.thrusters.length} thrusters actual power is ${thrustersActualPower.join(', ')}. <br>Rocket maximum power is ${totalmaxPower}. <br>Thrusters maximum power is ${thrustersPower.join(', ')}.`; 
-}
 
 
 
